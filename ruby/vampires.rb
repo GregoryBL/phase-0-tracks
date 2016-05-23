c7d76eb0bace921667953c8fbbd60e67b6578a9e
Gregory Berns-Leone <gberns.l@gmail.com>
2016-05-23 09:12:23 -0500

    Add questions and inputs

---
+++ ruby/vampires.rb
@@ -0,0 +1,19 @@
+puts "What is your name?"
+name = gets.chomp
+puts name
+
+puts "How old are you?"
+age = gets.chomp
+puts age
+
+puts "What year were you born?"
+year_born = gets.chomp
+puts year_born
+
+puts "Our company serves garlic bread. Should we order some for you?"
+order_garlic_bread = gets.chomp
+puts order_garlic_bread
+
+puts "Would you like to enroll in the company's health insurance?"
+health_insurance = gets.chomp
+puts health_insurance
