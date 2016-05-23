9c4371c9eb75edbb5314a82ea1d1896d2e6d8ba4
Gregory Berns-Leone <gberns.l@gmail.com>
2016-05-23 10:02:10 -0500

    Add loop for multiple employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,46 +1,54 @@
-puts "What is your name?"
-name = gets.chomp
-# puts name
+puts "How many employees do you want to process?"
+num_empl_left = gets.chomp.to_i
 
-puts "How old are you?"
-age = gets.chomp
-# puts age
+while num_empl_left > 0
 
-puts "What year were you born?"
-year_born = gets.chomp
-# puts year_born
+  puts "What is your name?"
+  name = gets.chomp
+  # puts name
 
-puts "Our company serves garlic bread. Should we order some for you?"
-order_garlic_bread = gets.chomp
-# puts order_garlic_bread
+  puts "How old are you?"
+  age = gets.chomp.to_i
+  # puts age
 
-puts "Would you like to enroll in the company's health insurance?"
-health_insurance = gets.chomp
-# puts health_insurance
+  puts "What year were you born?"
+  year_born = gets.chomp.to_i
+  # puts year_born
 
-result = ""
-age_correct = ( age.to_i == ( 2016 - year_born.to_i ))
-willing_to_eat_garlic = order_garlic_bread.downcase == "yes"
-enroll_health_insurance = health_insurance.downcase == "yes"
-name_suspicious = ( name.downcase == "drake cula" ) || ( name.downcase == "tu fang" )
+  puts "Our company serves garlic bread. Should we order some for you?"
+  order_garlic_bread = gets.chomp
+  # puts order_garlic_bread
 
-# Results inconclusive set in first one and will carry through til the end unless another one is true.
-if age_correct && ( willing_to_eat_garlic || enroll_health_insurance )
-  result = "Probably not a vampire."
-else
-  result = "Results inconclusive."
+  puts "Would you like to enroll in the company's health insurance?"
+  health_insurance = gets.chomp
+  # puts health_insurance
+
+  result = ""
+  age_correct = ( age == ( 2016 - year_born ))
+  willing_to_eat_garlic = order_garlic_bread.downcase == "yes"
+  enroll_health_insurance = health_insurance.downcase == "yes"
+  name_suspicious = ( name.downcase == "drake cula" ) || ( name.downcase == "tu fang" )
+
+  # Results inconclusive set in first one and will carry through til the end unless another one is true.
+  if age_correct && ( willing_to_eat_garlic || enroll_health_insurance )
+    result = "Probably not a vampire."
+  else
+    result = "Results inconclusive."
+  end
+
+  if !age_correct && ( !willing_to_eat_garlic || !enroll_health_insurance )
+    result = "Probably a vampire."
+  end
+
+  if !age_correct && !willing_to_eat_garlic && !enroll_health_insurance
+    result = "Almost certainly a vampire."
+  end
+
+  if name_suspicious
+    result = "Definitely a vampire."
+  end
+
+  puts result
+
+  num_empl_left = num_empl_left - 1
 end
-
-if !age_correct && ( !willing_to_eat_garlic || !enroll_health_insurance )
-  result = "Probably a vampire."
-end
-
-if !age_correct && !willing_to_eat_garlic && !enroll_health_insurance
-  result = "Almost certainly a vampire."
-end
-
-if name_suspicious
-  result = "Definitely a vampire."
-end
-
-puts result
