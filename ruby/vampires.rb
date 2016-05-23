16a4619d6fd51b37f0342751289f9122a3add35b
Gregory Berns-Leone <gberns.l@gmail.com>
2016-05-23 09:50:58 -0500

    Add vampire answer checks

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,19 +1,46 @@
 puts "What is your name?"
 name = gets.chomp
-puts name
+# puts name
 
 puts "How old are you?"
 age = gets.chomp
-puts age
+# puts age
 
 puts "What year were you born?"
 year_born = gets.chomp
-puts year_born
+# puts year_born
 
 puts "Our company serves garlic bread. Should we order some for you?"
 order_garlic_bread = gets.chomp
-puts order_garlic_bread
+# puts order_garlic_bread
 
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
-puts health_insurance
+# puts health_insurance
+
+result = ""
+age_correct = ( age.to_i == ( 2016 - year_born.to_i ))
+willing_to_eat_garlic = order_garlic_bread.downcase == "yes"
+enroll_health_insurance = health_insurance.downcase == "yes"
+name_suspicious = ( name.downcase == "drake cula" ) || ( name.downcase == "tu fang" )
+
+# Results inconclusive set in first one and will carry through til the end unless another one is true.
+if age_correct && ( willing_to_eat_garlic || enroll_health_insurance )
+  result = "Probably not a vampire."
+else
+  result = "Results inconclusive."
+end
+
+if !age_correct && ( !willing_to_eat_garlic || !enroll_health_insurance )
+  result = "Probably a vampire."
+end
+
+if !age_correct && !willing_to_eat_garlic && !enroll_health_insurance
+  result = "Almost certainly a vampire."
+end
+
+if name_suspicious
+  result = "Definitely a vampire."
+end
+
+puts result
