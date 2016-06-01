13d1b1d4d1fd6f674d0fcce5310af1af6eedfc6a
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-01 10:24:03 -0500

    Add each and map calls

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -4,4 +4,17 @@ def method_that_takes_a_block
   puts "After the block"
 end
 
+numbers_array = ["1", "2", "3", "4", "5"]
+letters_hash = {first: "a", second: "b", third: "c"}
+
 method_that_takes_a_block { |item1, item2| puts "Block was given #{item1} and #{item2}"}
+
+p numbers_array
+numbers_array.each { |number| puts "We've reached the number #{number}" }
+p numbers_array
+numbers_array.map! { |number| number.to_i + 1 }
+p numbers_array
+
+p letters_hash
+letters_hash.each { |ordinal, letter| puts "The #{ordinal} letter is #{letter}" }
+p letters_hash
