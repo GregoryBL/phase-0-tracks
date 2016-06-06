1347761d85367b89f6a592ebace8781071faa762
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-06 11:11:33 -0500

    Add instance variables and tests

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,11 +7,28 @@ class Santa
     puts "That was a good #{cookie_type}!"
   end
 
-  def initialize
+  def initialize(gender, ethnicity)
     puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
 end
 
-test_santa = Santa.new
+# TESTS
+
+test_santa = Santa.new("gender", "ethnicity")
 test_santa.eat_milk_and_cookies("chocolate chip cookie")
 test_santa.speak
+
+santas = []
+
+genders = ["male", "female", "N/A", "fluid"]
+ethnicities = ["Latino", "American Indian", "Martian", "Wood Elf"]
+
+genders.each_with_index do |gender, ind|
+  santas << Santa.new(gender, ethnicities[ind])
+end
+
+p santas
