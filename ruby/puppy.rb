f60c062aff56a39fbb844419ac8741e304ac5c4e
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-06 09:02:14 -0500

    Add puppy class schematic

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# Puppy
+
+# SPECIES ------------------
+# Canis familiaris
+
+# Characteristics ----------
+# Playfulness: varies
+# Friendly: yes
+# Eyes: 2
+# Name: varies
+# Color: varies
+
+# BEHAVIOR -----------------
+# Roll
+# Bark
+# Chew shoes
