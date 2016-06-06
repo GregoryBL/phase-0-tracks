ed1e9b2ba8aa52a168a36a95b1be656ad3856816
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-06 13:36:46 -0500

    Add pseudocode of go class

---
+++ ruby/solo.rb
@@ -0,0 +1,14 @@
+## Go board class
+
+### Attributes
+# Size
+# Points Array
+# Material
+# Moves made
+# Number of Stones
+
+### Methods
+# Play stone (color)
+# Remove stone
+# Remove all stones
+
