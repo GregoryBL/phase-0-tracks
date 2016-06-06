d9ed376c553d9e40d0d0b514ab14d94cfb58fc32
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-06 17:55:25 -0500

    Convert Shout to a mixin and use

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,40 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words.upcase + "!!!" + " :D"
+#   end
+# end
+
+# puts Shout.yell_angrily("Go away")
+
+# puts Shout.yelling_happily("Hello")
+
 module Shout
-  def self.yell_angrily(words)
+  def yell_angrily(words)
     words + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words.upcase + "!!!" + " :D"
   end
 end
 
-puts Shout.yell_angrily("Go away")
+class Human
+  include Shout
+end
 
-puts Shout.yelling_happily("Hello")
+class Goat
+  include Shout
+end
+
+leul = Human.new()
+goat = Goat.new()
+
+puts leul.yelling_happily("My name is Leul")
+puts goat.yelling_happily("blaaaaaaaa")
+
+puts leul.yell_angrily("Greg do the dishes")
+puts goat.yell_angrily("blaaaaaaaa")
