3edb8df2b4acde02de0508d765d16195159fdfe3
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-13 14:51:56 -0500

    Add pseudocode for longestString

---
+++ js/algos.js
@@ -0,0 +1,11 @@
+// Take in an array of strings
+// Make a variable longestStringLength = 0
+// Make a variable indexOfLongestString = null
+// For each string in the array:
+//   Find it's length
+//   If the length is longer than longestStringLength
+//     store it's length in longestStringLength
+//     update the indexOfLongestString to equal the current index
+// Return array[indexOfLongestString]
+
+
