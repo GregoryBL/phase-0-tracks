9aa1c9e9c07323af77947168f2fea5ae71432aac
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-13 14:58:36 -0500

    Add longestString and tests

--- js/algos.js
+++ js/algos.js
@@ -8,4 +8,28 @@
 //     update the indexOfLongestString to equal the current index
 // Return array[indexOfLongestString]
 
+function longestString(stringArray) {
+  var longestStringLength = 0;
+  var indexOfLongestString = null;
+  var arrayLength = stringArray.length;
+  if (arrayLength == 0) {
+    return "No strings in array.";
+  } 
 
+  for (i=0; i<arrayLength; i++) {
+    if (stringArray[i].length > longestStringLength) {
+      longestStringLength = stringArray[i].length;
+      indexOfLongestString = i;
+    }
+  }
+
+  if (indexOfLongestString == null) {
+    return "No strings of greater than zero length.";
+  }
+
+  return stringArray[indexOfLongestString];
+}
+
+console.log(longestString(["O", "Tw", "Thr", "four"])); // "four"
+console.log(longestString([])); // "No strings in array."
+console.log(longestString(["", ""])); // "No strings of greater than zero length."
