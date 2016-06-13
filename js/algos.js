93bc7e5d5159d9183c3d1ed0993ce13f46ba913c
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-13 15:51:30 -0500

    Add nRandomWords function

--- js/algos.js
+++ js/algos.js
@@ -47,6 +47,37 @@ function objectsHaveACommonPair(obj1, obj2) {
   return false;
 }
 
+// Take in an integer (number)
+// Make an alphabet string "abcdefghijklmnopqrstuvwxyz"
+// Create a returnArray = []
+// number times:
+//   generate a random number between 1 and 10 called stringLength
+//   create newString = ''
+//   stringLength times:
+//     Generate a random number between 0 and alphabet length
+//     Add that alphabet[randomNumber] to newString
+//   add newString to returnArray
+// return returnArray
+
+// helper function to abstract away generating a random number between min and max
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function nRandomWords(number) {
+  var alphabet = "abcdefghijklmnopqrstuvwxyz";
+  var returnArray = [];
+  for (i=0; i<number; i++) {
+    var stringLength = getRandomInt(1, 11);
+    var newString = '';
+    for (j=0; j<stringLength; j++) {
+      newString = newString + alphabet[getRandomInt(0, 26)];
+    }
+    returnArray.push(newString);
+  }
+  return returnArray;
+}
+
 // TESTS
 
 console.log(longestString(["O", "Tw", "Thr", "four"])); // "four"
@@ -56,3 +87,6 @@ console.log(longestString(["", ""])); // "No strings of greater than zero length
 console.log(objectsHaveACommonPair({ val1: "val1", val2: "val2"}, {val1: "val1", val2: "val1"})); // true
 console.log(objectsHaveACommonPair({ val1: "val1", val2: "val2"}, {val1: "val2", val2: "val1"})); // false
 console.log(objectsHaveACommonPair({ val1: "val1", val2: "val2"}, {})); // false
+
+console.log(nRandomWords(10));
+
