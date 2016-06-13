0acad17342de1349df0f44df011658b4dbb53da8
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-13 14:27:59 -0500

    Add pseudocode and reverse function

---
+++ js/explore.js
@@ -0,0 +1,20 @@
+// Reverse function that takes a string and reverses it
+// Start with an empty string in returnString
+// Get the string length
+// In a for loop (for each letter in the string), counting down by index:
+//   Take the index-th letter of the string
+//   Concatinate it to the return string and save it in returnString
+// Return returnString
+
+function reverse(string) {
+  length = string.length;
+  returnString = '';
+
+  for (i=length; i > 0; i--) {
+    returnString = returnString + string[i-1];
+  }
+  return returnString;
+}
+
+// Test
+console.log(reverse("Testing String"));
