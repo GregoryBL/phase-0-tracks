e8cb69ead20d12b7f2b2fd2520ef96d96692c581
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-14 10:40:44 -0500

    Add Car constructor and a couple of cars

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,3 +15,28 @@ for (var i =0; i<colors.length; i++) {
 }
 
 console.log(combo);
+
+function Car(color, numberOfWheels, topSpeed) {
+  this.color = color;
+  this.numberOfWheels = numberOfWheels;
+  this.topSpeed = topSpeed;
+
+  this.turnOn = function() {
+    console.log("Vroom");
+  }
+}
+
+var corvette = new Car("red", 4, 180);
+corvette.turnOn();
+console.log(corvette);
+
+var pinto = new Car("muddy yellow", 4, 85);
+pinto.turnOn();
+console.log(pinto);
+
+pinto.fallApart = function() {
+  console.log("I won't go any more.");
+}
+
+pinto.fallApart();
+// corvette.fallApart(); // Not a function
