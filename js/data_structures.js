c491d859191070715a8276e8a9b7aa085363d73a
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-14 10:29:08 -0500

    Copy code from pair

---
+++ js/data_structures.js
@@ -0,0 +1,17 @@
+var colors = ['blue', 'red','green','black'];
+
+var horseName = ['bucky','chuck','chief','hugo'];
+
+colors.push('white');
+horseName.push('barry');
+
+console.log(colors);
+console.log(horseName);
+
+var combo = {};
+
+for (var i =0; i<colors.length; i++) {
+    combo[horseName[i]]=colors[i];
+}
+
+console.log(combo);
