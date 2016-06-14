056a9913b8e67669fffb9a294b7425878a5a9ce1
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-14 12:49:49 -0500

    Add some code further demonstrating properties and about enumerating properties

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,19 +11,23 @@ console.log(horseName);
 var combo = {};
 
 for (var i =0; i<colors.length; i++) {
-    combo[horseName[i]]=colors[i];
+   combo[horseName[i]]=colors[i];
 }
 
 console.log(combo);
 
 function Car(color, numberOfWheels, topSpeed) {
-  this.color = color;
-  this.numberOfWheels = numberOfWheels;
-  this.topSpeed = topSpeed;
+ this.color = color;
+ this.numberOfWheels = numberOfWheels;
+ this.topSpeed = topSpeed;
 
-  this.turnOn = function() {
-    console.log("Vroom");
-  }
+ this.turnOn = function() {
+   console.log("Vroom");
+ }
+
+ this.paintIt = function(color) {
+  this.color=color;
+ }
 }
 
 var corvette = new Car("red", 4, 180);
@@ -35,8 +39,25 @@ pinto.turnOn();
 console.log(pinto);
 
 pinto.fallApart = function() {
-  console.log("I won't go any more.");
+ console.log("I won't go any more.");
 }
 
 pinto.fallApart();
 // corvette.fallApart(); // Not a function
+
+pinto.paintIt('blue');
+
+console.log(pinto.color);
+
+pinto.color="crappy orange";
+
+console.log(pinto.color);
+
+// Loop through properties of an object:
+for (var prop in pinto) {
+  if (pinto.hasOwnProperty(prop)) {
+    console.log(prop);
+  } 
+}
+
+console.log(new Object)
