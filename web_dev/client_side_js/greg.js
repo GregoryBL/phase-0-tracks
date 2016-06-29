09947dd74e875ca1d60b320656bea22571f9712f
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-29 11:05:28 -0500

    Add onClick action

--- web_dev/client_side_js/greg.js
+++ web_dev/client_side_js/greg.js
@@ -11,9 +11,29 @@ var claire = document.createElement('p');
 claire.textContent = "I am going to be an awesome scientist.";
 document.body.appendChild(claire);
 
-/*
-var bodkids = document.body.childNodes;
-for (i = 0; i < bodkids.length(); i++) {
-  bodkids[i].style.color = '#0000FF';
+var button1 = document.createElement('INPUT');
+button1.setAttribute("type", "button");
+button1.setAttribute("name", "iowa-button");
+button1.value = "Iowa";
+document.body.appendChild(button1);
+
+function toggleCorn(target) {
+  if (img1.hidden) {
+    img1.hidden = false;
+  } else {
+    img1.hidden = true;
+  }
+
+  // img1.hidden = "true";
 }
-*/
+
+button1.addEventListener("click", toggleCorn);
+
+var bodkids = document.body.childNodes;
+console.log(bodkids);
+
+// for (var i = 0; i < 3; ++i) {
+//   console.log(bodkids[i]);
+//   bodkids.item(i).style.color = '#0000FF';
+// }
+
