32ef048869e5185ad925ca24f01ffebddc9aa527
Gregory Berns-Leone <gberns.l@gmail.com>
2016-06-30 10:33:16 -0500

    Add contact, great_job, add, and search routes

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -44,3 +44,36 @@ get '/students/:id' do
   student = db.execute("SELECT * FROM students WHERE id=?", [params[:id]])[0]
   student.to_s
 end
+
+get '/contact' do
+  "1234 Main Street, Chicago, IL 60657"
+end
+
+get '/great_job' do
+  if params[:name]
+    "Good job, #{params[:name]}!"
+  else
+    "Good job!"
+  end
+end
+
+get '/add/:num1/to/:num2' do
+  (params[:num1].to_f + params[:num2].to_f).to_s
+end
+
+get '/search' do
+  p params[:campus]
+  if params[:campus]
+    results = db.execute("SELECT * FROM students WHERE campus = ?", params[:campus])
+    p results
+  else
+    results = []
+  end
+  response = "<h1>Students from #{params[:campus]} campus:</h1>"
+  results.each do |student|
+    response << "ID: #{student['id']}<br>"
+    response << "Name: #{student['name']}<br>"
+    response << "Age: #{student['age']}<br><br>"
+  end
+  response
+end
